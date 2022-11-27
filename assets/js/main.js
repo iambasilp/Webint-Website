073a03623e087bfde9dcacdb0ba76da74ae22650
iambasilp <basilxg321@gmail.com>
2022-11-27 23:24:19 +0530

    Mobile created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,41 @@
 /*=============== SHOW MENU ===============*/
-
+const navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close"),
+  navMenu = document.getElementById("nav-menu");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
+const navLinks = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", linkAction);
+});
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // when scroll is greaterthan equalto 50 viewport the scroll-header class add to header
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
