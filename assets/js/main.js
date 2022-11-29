3029467fe150a1e0ea47a20eeff4f6616d237771
iambasilp <basilxg321@gmail.com>
2022-11-29 16:21:44 +0530

    Scroll reveal animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,6 +105,6 @@ const sr = ScrollReveal(
 )
 sr.reveal('.home__img, .newsletter__container,.footer__logo,.footer__description, .footer__content , .footer__info')
 sr.reveal('.home__data',{origin:"bottom"})
-sr.reveal('.about__img, .recently__img, .newsletter__form',{origin:"right"})
-sr.reveal('.about__data , .recently__data',{origin:"left"})
+sr.reveal('.about__img, .recently__img',{origin:"right"})
+sr.reveal('.about__data , .recently__data , .newsletter__button',{origin:"left"})
 sr.reveal('.popular__card',{interval:60})
