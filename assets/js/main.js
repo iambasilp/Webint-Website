c88001a32785d3a3deecba53399f5bc36079fb53
iambasilp <basilxg321@gmail.com>
2022-11-30 18:33:04 +0530

    Javascript updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ const scrollActive = () => {
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       sectionClass.classList.add("active-link");
     } else {
-      sectionClass.classList.remove('active-link')
+      sectionClass.classList.remove("active-link");
     }
   });
 };
@@ -63,48 +63,56 @@ const scrollActive = () => {
 window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
- 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal(
-  {
-    origin:"top",
-    distance:"60px",
-    duration:2500,
-    delay:400,
-    // reset: true // animation will repeate
-  }
-)
-sr.reveal('.home__img, .newsletter__container,.footer__logo,.footer__description, .footer__content , .footer__info')
-sr.reveal('.home__data',{origin:"bottom"})
-sr.reveal('.about__img, .recently__img',{origin:"right"})
-sr.reveal('.about__data , .recently__data , .newsletter__button',{origin:"left"})
-sr.reveal('.books__card',{interval:70})
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true // animation will repeate
+});
+sr.reveal(
+  ".home__img, .newsletter__container,.footer__logo,.footer__description, .footer__content , .footer__info"
+);
+sr.reveal(".home__data", { origin: "bottom" });
+sr.reveal(".about__img, .recently__img", { origin: "right" });
+sr.reveal(".about__data , .recently__data , .newsletter__button", {
+  origin: "left",
+});
+sr.reveal(".books__card", { interval: 70 });
